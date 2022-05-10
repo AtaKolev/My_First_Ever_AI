54a2f4a99e968bdc68b532a808cf2452c08414ab
AtaKolev <atanaskolevv01@gmail.com>
2022-05-10 15:18:02 +0300

    Initial commit

---
+++ Code_Agent.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 15:24:39 2021
+
+@author: atana
+"""
+
---
+++ Labyrinth_generator.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 28 15:26:07 2021
+
+@author: atana
+"""
+
