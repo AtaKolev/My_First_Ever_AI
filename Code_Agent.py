3524bf6af754e17485e0e61e63f50ca64928615f
AtaKolev <atanaskolevv01@gmail.com>
2022-05-10 16:02:24 +0300

    Init commit
    Init method on the agent

--- Code_Agent.py
+++ Code_Agent.py
@@ -5,3 +5,74 @@ Created on Thu Oct 28 15:24:39 2021
 @author: atana
 """
 
+import numpy as np
+
+class Agent:
+    
+    def __init__(self, starting_position, grid, gamma):
+        self.position = starting_position
+        self.states = []
+        self.R_states = []
+        self.S_states = []
+        for i in range(len(grid)-1):
+            for j in range(len(grid)-1):
+                if grid[i, j] == ' ':
+                    self.states.append([i, j])
+                else:
+                    if grid[i, j] == 'R':
+                        self.R_states.append([i, j])
+                    else:
+                        if grid[i, j] == 'S':
+                            self.S_states.append([i, j])
+                        else:
+                            continue
+        self.gamma = gamma
+        self.grid_size = len(grid)
+        self.action_probability = {"N" : 0.166,
+                                   "E" : 0.166,
+                                   "S" : 0.166,
+                                   "W" : 0.166,
+                                   "NE" : 0.083,
+                                   "NW" : 0.083,
+                                   "SE" : 0.083,
+                                   "SW" : 0.083}
+        self.special_case_probability = {"N" : 0.2,
+                                         "E" : 0.1143,
+                                         "S" : 0.1143,
+                                         "W" : 0.1143,
+                                         "NE" : 0.1143,
+                                         "NW" : 0.1143,
+                                         "SE" : 0.1143,
+                                         "SW" : 0.1143}
+        self.actions = list(self.action_probability.keys())
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- Labyrinth_generator.py
+++ Labyrinth_generator.py
@@ -5,3 +5,52 @@ Created on Thu Oct 28 15:26:07 2021
 @author: atana
 """
 
+import numpy as np
+
+def lab_gen():
+
+    x_y_dict = {'x' : np.arange(0,11),
+                'y' : np.arange(0, 11)}
+    lab_map_disp = []
+
+    for y in range(len(x_y_dict['y'])-1, -1, -1):
+        temp = []
+        for x in range(0, len(x_y_dict['x'])):
+            if x == 0 and (y in np.arange(0, 10)):
+                if x == 0 and y == 1:
+                    temp.append('O') # Entry point of the agent
+                else:
+                    temp.append('|')
+            elif x == 10 and (y in np.arange(0, 10)):
+                temp.append('|')
+            elif y == 0 and (x in np.arange(0, 11)):
+                temp.append('-')
+            elif y == 10 and (x in np.arange(0, 11)):
+                temp.append('-')
+            elif x == 2 and (y in np.arange(1, 9)):
+                temp.append('|')
+            elif (x in np.arange(3, 6)) and y == 8:
+                temp.append('-')
+            elif x == 5 and (y in np.arange(2, 9)):
+                temp.append('|')
+            elif x == 4 and y == 2:
+                temp.append('L')
+            elif x == 4 and (y in np.arange(3, 9)):
+                temp.append('|')
+            elif (x == 3 and y == 7) or (x == 9 and y == 9):
+                temp.append('S')
+            elif (x in np.arange(7, 10) and y == 8):
+                temp.append('-')
+            elif x == 7 and (y in np.arange(2, 8)):
+                temp.append('|')
+            elif x == 8 and y == 2:
+                temp.append('⅃')
+            elif x == 8 and (y in np.arange(3, 8)):
+                temp.append('|')
+            elif x == 9 and y == 7:
+                temp.append('R')
+            else:
+                temp.append(' ')
+        lab_map_disp.append(temp)
+
+    return lab_map_disp
